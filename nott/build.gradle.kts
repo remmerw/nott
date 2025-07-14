3cb1c06cecd9d79b709f38300f0730d5bbf123e4
remmerw <remmer.wilts@gmail.com>
2025-07-14 11:27:51 +0200

    Version 0.0.1 ------------- Skeleton

---
+++ nott/build.gradle.kts
@@ -0,0 +1,113 @@
+@file:OptIn(ExperimentalWasmDsl::class, ExperimentalKotlinGradlePluginApi::class)
+
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.vanniktech.mavenPublish)
+}
+
+group = "io.github.remmerw"
+version = "0.0.1"
+
+kotlin {
+
+
+    androidTarget {
+        instrumentedTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
+        publishLibraryVariants("release")
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
+
+
+    jvm()
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+
+    // linuxArm64()
+    // linuxX64()
+    // linuxArm64()
+    // wasmJs()
+    // wasmWasi()
+    // js()
+
+    sourceSets {
+        commonMain {
+            dependencies {
+                implementation(libs.kotlin.stdlib)
+                implementation(libs.ktor.network)
+            }
+        }
+
+        commonTest {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+
+        androidInstrumentedTest.dependencies {
+            implementation(libs.kotlin.test)
+            implementation("androidx.test:runner:1.6.2")
+        }
+
+        androidUnitTest.dependencies {
+            implementation(libs.kotlin.test)
+        }
+    }
+}
+
+
+android {
+    namespace = "io.github.remmerw.nott"
+    compileSdk = 36
+    defaultConfig {
+        minSdk = 27
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+}
+
+
+mavenPublishing {
+    publishToMavenCentral()
+
+    signAllPublications()
+
+    coordinates(group.toString(), "nott", version.toString())
+
+    pom {
+        name = "nott"
+        description = "Read Only DHT node"
+        inceptionYear = "2025"
+        url = "https://github.com/remmerw/nott/"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
+            }
+        }
+        developers {
+            developer {
+                id = "remmerw"
+                name = "Remmer Wilts"
+                url = "https://github.com/remmerw/"
+            }
+        }
+        scm {
+            url = "https://github.com/remmerw/nott/"
+            connection = "scm:git:git://github.com/remmerw/nott.git"
+            developerConnection = "scm:git:ssh://git@github.com/remmerw/nott.git"
+        }
+    }
+}
