632a076005335df287889aa6c8ba70756f3123b0
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-14 20:10:27 +0545

    Aktualisieren von RequestGetPeers.kt

--- nott/src/commonMain/kotlin/io/github/remmerw/nott/RequestGetPeers.kt
+++ nott/src/commonMain/kotlin/io/github/remmerw/nott/RequestGetPeers.kt
@@ -21,7 +21,7 @@ fun CoroutineScope.requestGetPeers(
     intermediateTimeout: () -> Long,
 ): ReceiveChannel<PeerResponse> =
     produce {
-        val gated: MutableSet<InetSocketAddress> = sortedSetOf()
+        val gated: MutableSet<Int> = sortedSetOf()
 
         while (true) {
             val closest = ClosestSet(nott, target)
@@ -65,7 +65,7 @@ fun CoroutineScope.requestGetPeers(
 
                             val list = mutableListOf<InetSocketAddress>()
                             for (item in message.values) {
-                                if (gated.add(item)) {
+                                if (gated.add(item.hashCode())) {
                                     list.add(item)
                                 }
                             }
