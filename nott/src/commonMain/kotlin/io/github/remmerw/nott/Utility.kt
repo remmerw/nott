53a69214a1812341b9fe3ac20ea87de0077dbfe3
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-15 18:25:54 +0545

    Aktualisieren von Utility.kt

--- nott/src/commonMain/kotlin/io/github/remmerw/nott/Utility.kt
+++ nott/src/commonMain/kotlin/io/github/remmerw/nott/Utility.kt
@@ -3,16 +3,8 @@ package io.github.remmerw.nott
 import java.util.Arrays
 import kotlin.math.min
 
-internal fun ByteArray.toLongKey(length: Int = 8): Long {
-    require(length in 1..8) { "Length must be between 1 and 8 bytes for a Long value." }
-    require(this.size >= length) { "Array is too small for the requested length." }
-
-    var result = 0L
-    for (i in 0 until length) {
-        val byteValue = this[i].toLong() and 0xFFL
-        result = (result shl 8) or byteValue
-    }
-    return result
+internal fun ByteArray.toLongKey(): Long {
+    return this.toLong()
 }
 
 internal fun ByteArray.toLong(length: Int = 8): Long {
