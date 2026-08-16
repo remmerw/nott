764adc152482483077cc2f65f24beca7e4cce8fc
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-16 17:00:05 +0545

    Aktualisieren von Address.kt

--- nott/src/commonMain/kotlin/io/github/remmerw/nott/Address.kt
+++ nott/src/commonMain/kotlin/io/github/remmerw/nott/Address.kt
@@ -3,5 +3,23 @@ package io.github.remmerw.nott
 @Suppress("ArrayInDataClass")
 internal data class Address(
     val address: ByteArray,
-    val port: UShort){
+    val port: UShort
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Address
+
+        if (!address.contentEquals(other.address)) return false
+        if (port != other.port) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = address.contentHashCode()
+        result = 31 * result + port.hashCode()
+        return result
+    }
 }
