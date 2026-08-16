37a64b545f1e7f80af82ab7a6a34ab6ecacea887
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-16 21:08:40 +0545

    Aktualisieren von Utility.kt

--- nott/src/commonMain/kotlin/io/github/remmerw/nott/Utility.kt
+++ nott/src/commonMain/kotlin/io/github/remmerw/nott/Utility.kt
@@ -5,6 +5,10 @@ import java.net.InetSocketAddress
 import java.util.Arrays
 import kotlin.math.min
 
+fun InetSocketAddress.toAddress(): Address {
+     return Address(this.address.address, this.port.toUShort())
+}
+
 internal fun Address.toInetSocketAddress(): InetSocketAddress {
     val inetAddress = InetAddress.getByAddress(this.address)
 
