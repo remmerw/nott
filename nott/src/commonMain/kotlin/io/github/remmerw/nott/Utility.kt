e252926c6426d1f5a81ff6b829e7b5a60f23bcfe
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-17 14:50:30 +0545

    Aktualisieren von Utility.kt

--- nott/src/commonMain/kotlin/io/github/remmerw/nott/Utility.kt
+++ nott/src/commonMain/kotlin/io/github/remmerw/nott/Utility.kt
@@ -5,15 +5,15 @@ import java.net.InetSocketAddress
 import java.util.Arrays
 import kotlin.math.min
 
-internal fun InetSocketAddress.toAddress(): Address = Address(this.address.address, this.port.toUShort())
+fun InetSocketAddress.toAddress(): Address = Address(this.address.address, this.port.toUShort())
 
-internal fun Address.toInetSocketAddress(): InetSocketAddress {
+fun Address.toInetSocketAddress(): InetSocketAddress {
     val inetAddress = InetAddress.getByAddress(this.address)
 
     return InetSocketAddress(inetAddress, this.port.toInt())
 }
 
-internal fun Address.toInetAddress(): InetAddress = InetAddress.getByAddress(this.address)
+fun Address.toInetAddress(): InetAddress = InetAddress.getByAddress(this.address)
 
 internal fun ByteArray.toLong(length: Int = 8): Long {
     require(length in 1..8) { "Length must be between 1 and 8 bytes for a Long value." }
