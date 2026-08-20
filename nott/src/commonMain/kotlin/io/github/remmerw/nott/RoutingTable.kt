843c46b8114c7e6e7502252e3d0ac4641d0924ec
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-20 15:56:08 +0545

    Aktualisieren von RoutingTable.kt

--- nott/src/commonMain/kotlin/io/github/remmerw/nott/RoutingTable.kt
+++ nott/src/commonMain/kotlin/io/github/remmerw/nott/RoutingTable.kt
@@ -7,47 +7,26 @@ internal class RoutingTable internal constructor() {
     // but it is not yet really necessary (not enough peers in the routing table)
     private val entries: MutableMap<Long, Peer> = ConcurrentHashMap()
 
-    fun insertOrRefresh(peer: Peer) {
-        val entry = entries[peer.key()]
-        if (entry != null) {
-            refresh(peer)
-        } else {
-            entries[peer.key()] = peer
-        }
-    }
-
-    fun refresh(peer: Peer) {
-        entries[peer.key()]
-            ?.mergeInTimestamps(peer)
+    fun insert(peer: Peer) {
+        entries[peer.key()] = peer
     }
 
     fun closestPeers(
         key: ByteArray,
         take: Int,
     ): Set<Peer> =
-        entries()
-            .filter { peer -> peer.eligibleForNodesList() }
-            .sortedWith { a, b ->
+        entries().sortedWith { a, b ->
                 threeWayDistance(key, a.id, b.id)
             }.take(take)
             .toSet()
 
     fun entries(): List<Peer> = entries.values.toList()
 
-    fun onTimeout(id: ByteArray) {
-        val peer = entries[id.toLong()]
-        if (peer != null) {
-            peer.signalFailure()
-            // only removes the entry if it is bad
-            if (peer.needsReplacement()) {
-                entries.remove(peer.key())
-            }
-        }
+    fun remove(id: ByteArray) {
+        entries.remove(id.toLong())
     }
 
     fun findPeerById(id: ByteArray): Peer? = entries[id.toLong()]
 
-    fun notifyOfResponse(msg: Message) {
-        entries[msg.id.toLong()]?.signalResponse()
-    }
+
 }
