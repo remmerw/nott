c3bc481e87c682212ff145119f9ef921b21251e0
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-20 17:21:38 +0545

    Aktualisieren von ClosestSet.kt

--- nott/src/commonMain/kotlin/io/github/remmerw/nott/ClosestSet.kt
+++ nott/src/commonMain/kotlin/io/github/remmerw/nott/ClosestSet.kt
@@ -96,8 +96,8 @@ internal class ClosestSet(
         if (match != null) {
             val message = call.response
             if (message is NodesResponse) {
-                addCandidates(message.nodes6)
-                addCandidates(message.nodes)
+                addCandidates(message.nodes6.toSet())
+                addCandidates(message.nodes.toSet())
             }
         }
         return match
