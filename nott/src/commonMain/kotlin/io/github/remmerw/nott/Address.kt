b8e6c22dda5bcdc5197ddbc17c2391507855bc64
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-23 13:13:12 +0545

    Aktualisieren von Address.kt

--- nott/src/commonMain/kotlin/io/github/remmerw/nott/Address.kt
+++ nott/src/commonMain/kotlin/io/github/remmerw/nott/Address.kt
@@ -4,21 +4,23 @@ import java.net.InetAddress
 import java.net.InetSocketAddress
 
 sealed interface Address {
-    val address: ByteArray,
-    val port: UShort,
+    val address: ByteArray
+    val port: UShort
 
     fun inetAddress(): InetAddress
     fun inetSocketAddress(): InetSocketAddress 
 }
+
 @Suppress("ArrayInDataClass")
 internal data class RawAddress(
     override val address: ByteArray,
     override val port: UShort,
 ) : Address {
     private val inetAddress: InetAddress by lazy { InetAddress.getByAddress(address) }
-    
-    private val inetSocketAddress: InetSocketAddress by lazy { InetSocketAddress(inetAddress(), port.toInt()
-     }
+
+    private val inetSocketAddress: InetSocketAddress by lazy { 
+        InetSocketAddress(inetAddress(), port.toInt()) 
+    }
 
     override fun inetAddress(): InetAddress = inetAddress
 
@@ -28,7 +30,7 @@ internal data class RawAddress(
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
-        other as Address
+        other as RawAddress
 
         if (!address.contentEquals(other.address)) return false
         if (port != other.port) return false
@@ -44,9 +46,11 @@ internal data class RawAddress(
 }
 
 fun createAddress(iso: InetSocketAddress): Address { 
-    return RawAddress(iso.address.address, iso.port.toUShort()
+    return RawAddress(iso.address.address, iso.port.toUShort())
 }
 
 fun createAddress(address: ByteArray, port: UShort): Address {
-    return RawAddress(address,port)
+    return RawAddress(address, port)
 }
+
+
