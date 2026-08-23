4971a7138c75d0314cb3c4b7b3ae7e353bd07ba1
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-23 15:24:40 +0545

    Aktualisieren von Network.kt

--- nott/src/commonTest/kotlin/io/github/remmerw/nott/Network.kt
+++ nott/src/commonTest/kotlin/io/github/remmerw/nott/Network.kt
@@ -1,11 +1,11 @@
+package io.github.remmerw.nott
+
 
 import java.net.InetAddress
 
-
-
 fun internet() : Boolean{
         return try {
-            // Ping Google's DNS server (timeout after 2000ms)
+            
             InetAddress.getByName("8.8.8.8").isReachable(2000)
         } catch (e: Exception) {
             false
